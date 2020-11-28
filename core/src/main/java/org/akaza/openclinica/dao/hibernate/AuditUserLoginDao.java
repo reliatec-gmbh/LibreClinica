3b4feb92e62eafa284084221bc1e3ab55ff82ac3
tomhickerson <tomh@xig.li>
2020-11-28 15:05:24 +0000

    first stab at fixing upstream issue

--- core/src/main/java/org/akaza/openclinica/dao/hibernate/AuditUserLoginDao.java
+++ core/src/main/java/org/akaza/openclinica/dao/hibernate/AuditUserLoginDao.java
@@ -38,7 +38,11 @@ public class AuditUserLoginDao extends AbstractDomainDao<AuditUserLoginBean> {
     public ArrayList<AuditUserLoginBean> getWithFilterAndSort(final AuditUserLoginFilter filter, final AuditUserLoginSort sort, final int rowStart,
             final int rowEnd) {
         Criteria criteria = getCurrentSession().createCriteria(domainClass());
-        criteria = filter.execute(criteria);
+        try {
+		criteria = filter.execute(criteria);
+	} catch (IllegalArgumentException ex) {
+		return new ArrayList<AuditUserLoginBean>();
+	}
         criteria = sort.execute(criteria);
         criteria.setFirstResult(rowStart);
         criteria.setMaxResults(rowEnd - rowStart);
