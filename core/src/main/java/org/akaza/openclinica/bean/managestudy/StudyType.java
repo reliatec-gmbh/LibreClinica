5730ff4a5f1e075da8f5da26ecbc18e6ae578ea3
Christian Hänsel <chae.reliatec@googlemail.com>
2020-12-08 17:40:50 +0100

    fixed different warnings for StudyType
    Signed-off-by: Christian Hänsel <chae.reliatec@googlemail.com>

--- core/src/main/java/org/akaza/openclinica/bean/managestudy/StudyType.java
+++ core/src/main/java/org/akaza/openclinica/bean/managestudy/StudyType.java
@@ -7,11 +7,12 @@
  */
 package org.akaza.openclinica.bean.managestudy;
 
-import org.akaza.openclinica.bean.core.Term;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+
+import org.akaza.openclinica.bean.core.Term;
 
 // Internationalized name and description in Term.getName and
 // Term.getDescription()
@@ -31,7 +32,7 @@ public class StudyType extends Term {
 
     private static final StudyType[] members = { GENETIC, NONGENETIC };
 
-    public static final List list = Arrays.asList(members);
+    public static final List<StudyType> list = Arrays.asList(members);
 
     private StudyType(int id, String name) {
         super(id, name);
@@ -42,15 +43,17 @@ public class StudyType extends Term {
     }
 
     public static StudyType get(int id) {
-        Term t = Term.get(id, list);
+    	Optional<StudyType> o = list.stream().filter(r -> r.getId() == id).findFirst();
+    	StudyType t = o.orElse(StudyType.INVALID);
+    	
         if (!t.isActive()) {
             return StudyType.INVALID;
         }
-        return (StudyType) t;
+        return t;
     }
 
-    public static ArrayList toArrayList() {
-        return new ArrayList(list);
+    public static ArrayList<StudyType> toArrayList() {
+        return new ArrayList<>(list);
     }
 
 }
