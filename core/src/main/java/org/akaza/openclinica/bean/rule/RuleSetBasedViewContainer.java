2b2811baae929ab616b87022c00377bca960b660
toskrip <tomas@skripcak.net>
2024-06-20 16:37:18 +0200

    Clean duplicated license notice comment

--- core/src/main/java/org/akaza/openclinica/bean/rule/RuleSetBasedViewContainer.java
+++ core/src/main/java/org/akaza/openclinica/bean/rule/RuleSetBasedViewContainer.java
@@ -7,13 +7,6 @@
  * copyright (C) 2003 - 2019 OpenClinica
  * copyright (C) 2020 - 2024 LibreClinica
  */
-/* 
- * LibreClinica is distributed under the
- * GNU Lesser General Public License (GNU LGPL).
- * For details see: https://libreclinica.org/license
- *
- * Copyright 2003-2008 Akaza Research 
- */
 package org.akaza.openclinica.bean.rule;
 
 import java.util.HashSet;
