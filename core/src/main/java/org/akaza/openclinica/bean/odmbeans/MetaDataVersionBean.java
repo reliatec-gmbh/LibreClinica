daca1cfa4c14587b271e666574018244e8c9992f
toskrip <tomas@skripcak.net>
2024-06-20 14:44:11 +0200

    Clean duplicated license notice comment

--- core/src/main/java/org/akaza/openclinica/bean/odmbeans/MetaDataVersionBean.java
+++ core/src/main/java/org/akaza/openclinica/bean/odmbeans/MetaDataVersionBean.java
@@ -7,15 +7,6 @@
  * copyright (C) 2003 - 2019 OpenClinica
  * copyright (C) 2020 - 2024 LibreClinica
  */
-/*
- * LibreClinica is distributed under the GNU Lesser General Public License (GNU
- * LGPL).
- *
- * For details see: https://libreclinica.org/license copyright 2003-2005 Akaza
- * Research
- *
- */
-
 package org.akaza.openclinica.bean.odmbeans;
 
 import org.akaza.openclinica.bean.managestudy.StudyBean;
