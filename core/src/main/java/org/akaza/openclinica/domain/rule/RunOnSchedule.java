2881d13f45936b84769bf9bd1b5eccaaabafe032
toskrip <tomas@skripcak.net>
2024-06-24 10:41:11 +0200

    Clean duplicated license notice comment

--- core/src/main/java/org/akaza/openclinica/domain/rule/RunOnSchedule.java
+++ core/src/main/java/org/akaza/openclinica/domain/rule/RunOnSchedule.java
@@ -13,33 +13,20 @@ public class RunOnSchedule {
 
 	private String runTime;
 
-	
 	public RunOnSchedule() {
 		super();
 	}
 
-
-	public RunOnSchedule(String runTime){
+	public RunOnSchedule(String runTime) {
 		this.runTime=runTime;
-		
 	}
-
 
 	public String getRunTime() {
 		return runTime;
 	}
 
-
 	public void setRunTime(String runTime) {
 		this.runTime = runTime;
 	}
 
-
-	
-	
-
-	
-	
-	
-	
 }
