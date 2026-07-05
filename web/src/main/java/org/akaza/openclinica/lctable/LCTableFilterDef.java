4adb3c6a602203b89b40789e979c20ae72e3904c
Giuseppe Del Castillo <constructum@web.de>
2026-07-05 09:26:54 +0200

    LCTable: variable width for columns now depends on max width of the text present in the column content (do not adapt column width to filter width; instead do the opposite).

--- web/src/main/java/org/akaza/openclinica/lctable/LCTableFilterDef.java
+++ web/src/main/java/org/akaza/openclinica/lctable/LCTableFilterDef.java
@@ -48,32 +48,34 @@ public abstract class LCTableFilterDef {
             final String filterName = LCTableParams.PARAM_FILTER_PREFIX + col.columnName;
             final String filterValue = ctx.filters.getOrDefault(col.columnName, "");
 
-            String trigger = "input changed delay:400ms";
+            final String trigger = "input changed delay:400ms";
+            final String triggerWithValidation = "input[this.validity.valid] changed delay:400ms";   // ONLY fire HTMX if the HTML5 validity state is 'valid'
 
-            var input = tr.td().input()
-                .attrType(EnumTypeInputType.TEXT)
-                .attrName(filterName)
-                .attrValue(filterValue)
-                .attrClass("filter-input")
-                .attrId(table.panelId + "-filter-" + col.columnName);
+            tr.td().div().attrClass("filter-wrapper").of(div -> {
+                var input = div.input()
+                    .attrType(EnumTypeInputType.TEXT)
+                    .attrName(filterName)
+                    .attrValue(filterValue)
+                    .attrClass("filter-input")
+                    .attrSize(1L)
+                    .attrId(table.panelId + "-filter-" + col.columnName);
 
-            // If a pattern is provided, add HTML5 validation and HTMX event filter
-            if (this.pattern != null) {
-                input.attrPattern(this.pattern);
-                if (this.message != null) {
-                    input.attrTitle(this.message);
+                // If a pattern is provided, add HTML5 validation and HTMX event filter
+                if (this.pattern != null) {
+                    input.attrPattern(this.pattern);
+                    if (this.message != null) {
+                        input.attrTitle(this.message);
+                    }
                 }
-                // ONLY fire HTMX if the HTML5 validity state is 'valid'
-                trigger = "input[this.validity.valid] changed delay:400ms";
-            }
 
-            input.addAttr(LCTable.HX_GET, table.entityPath)
-                .addAttr(LCTable.HX_TARGET, "#" + table.panelId)
-                .addAttr(LCTable.HX_SWAP, "outerHTML")
-                .addAttr(LCTable.HX_PUSH_URL, "true")
-                .addAttr(LCTable.HX_TRIGGER, trigger)
-                .addAttr(LCTable.HX_INCLUDE, "closest form")
-                .__().__();
+                input.addAttr(LCTable.HX_GET, table.entityPath)
+                    .addAttr(LCTable.HX_TARGET, "#" + table.panelId)
+                    .addAttr(LCTable.HX_SWAP, "outerHTML")
+                    .addAttr(LCTable.HX_PUSH_URL, "true")
+                    .addAttr(LCTable.HX_TRIGGER, this.pattern == null ? trigger : triggerWithValidation)
+                    .addAttr(LCTable.HX_INCLUDE, "closest form")
+                    .__().__();
+            }).__();
         }
     }
 
@@ -121,24 +123,26 @@ public abstract class LCTableFilterDef {
             // If it's invalid (e.g., "broken"), treat it as empty ("All") so the UI snaps back to a valid state
             final String currentSelected = isValidOption ? rawSelected : "";
 
-            org.xmlet.htmlapifaster.Select<Td<Tr<R>>> select = tr.td().select()
-                .attrName(filterName)
-                .attrId(table.panelId + "-filter-" + col.columnName)
-                .attrClass("filter-select")
-                .addAttr(LCTable.HX_GET, table.entityPath)
-                .addAttr(LCTable.HX_TARGET, "#" + table.panelId)
-                .addAttr(LCTable.HX_SWAP, "outerHTML")
-                .addAttr(LCTable.HX_PUSH_URL, "true")
-                .addAttr(LCTable.HX_TRIGGER, "change")
-                .addAttr(LCTable.HX_INCLUDE, "closest form");
+            tr.td().div().attrClass("filter-wrapper").of(div -> {
+                var select = div.select()
+                    .attrName(filterName)
+                    .attrId(table.panelId + "-filter-" + col.columnName)
+                    .attrClass("filter-select")
+                    .addAttr(LCTable.HX_GET, table.entityPath)
+                    .addAttr(LCTable.HX_TARGET, "#" + table.panelId)
+                    .addAttr(LCTable.HX_SWAP, "outerHTML")
+                    .addAttr(LCTable.HX_PUSH_URL, "true")
+                    .addAttr(LCTable.HX_TRIGGER, "change")
+                    .addAttr(LCTable.HX_INCLUDE, "closest form");
 
-            select.option().attrValue("").attrSelected(currentSelected.isEmpty()).text("").__();
-            this.values.forEach(option -> {
-                final String labelText = this.label(option);
-                boolean isSelected = labelText.equals(currentSelected);
-                select.option().attrValue(labelText).attrSelected(isSelected).text(labelText).__();
-            });
-            select.__().__();
+                select.option().attrValue("").attrSelected(currentSelected.isEmpty()).text("").__();
+                this.values.forEach(option -> {
+                    final String labelText = this.label(option);
+                    boolean isSelected = labelText.equals(currentSelected);
+                    select.option().attrValue(labelText).attrSelected(isSelected).text(labelText).__();
+                });
+                select.__().__();
+            }).__();
         }
     }
 
