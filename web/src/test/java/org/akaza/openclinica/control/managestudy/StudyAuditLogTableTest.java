d5629da48a729c47345bf5e789661799761ec3ac
Giuseppe Del Castillo <constructum@web.de>
2026-09-07 22:43:03 +0200

    StudyAuditLogTableTest: copyright year corrected.

--- web/src/test/java/org/akaza/openclinica/control/managestudy/StudyAuditLogTableTest.java
+++ web/src/test/java/org/akaza/openclinica/control/managestudy/StudyAuditLogTableTest.java
@@ -1,5 +1,9 @@
 /*
- * LibreClinica is distributed under the GNU Lesser General Public License (GNU LGPL).
+ * LibreClinica is distributed under the
+ * GNU Lesser General Public License (GNU LGPL).
+ *
+ * For details see: https://libreclinica.org/license
+ * copyright (C) 2026 LibreClinica
  */
 package org.akaza.openclinica.control.managestudy;
 
